f6c16fd486964e432d321ca64915ce52161e10a6
Ahmed Al Ashhab <96653465+dudeofmann@users.noreply.github.com>
2022-05-29 00:19:08 +0300

    Scroll into view and unhide has been implemented. Now the ability to click on elements conditionally has been implemented onto the card titles and Sidebar. Neet to fix bug to do with capsules and Section 4.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const About = () => {
-  return <div>Hello yesyt</div>;
+  return (
+    <section>
+      <h1>About</h1>
+      <p>
+        This is the about page for the website. This is where you will find
+        information about the company and the product.
+      </p>
+    </section>
+  );
 };
 
 export default About;
--- src/components/Create/Order.js
+++ src/components/Create/Order.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 const Order = () => {
+  const [isCapsuleSelected, setIsCapsuleSelected] = useState(false);
   const [quizState, setQuizState] = useState({});
+  const quizArrayLength = Object.entries(quizState).length;
   const [sectionCards, setSectionCards] = useState({
     sectionOne: {
-      "value_key": "process",
+      value_key: "process",
       activeIndex: null,
       title: "How do you drink your coffee?",
       sidebar_name: "Preferences",
@@ -32,7 +34,7 @@ const Order = () => {
       ],
     },
     sectionTwo: {
-      "value_key": "type",
+      value_key: "type",
       title: "What type of coffee?",
       sidebar_name: "Bean Type",
       activeIndex: null,
@@ -40,19 +42,19 @@ const Order = () => {
       hidden: true,
       cards: [
         {
-          id:"single",
+          id: "single",
           name: "Single Origin",
           description:
             "Distinct, high quality coffee from a specific family-owned farm",
         },
         {
-          id:"decaf",
+          id: "decaf",
           name: "Decaf",
           description:
             "Just like regular coffee, except the caffeine has been removed",
         },
         {
-          id:"blended",
+          id: "blended",
           name: "Blended",
           description:
             "Combination of two or three dark roasted beans of organic coffees",
@@ -60,27 +62,27 @@ const Order = () => {
       ],
     },
     sectionThree: {
-      "value_key": "quantity",
+      value_key: "quantity",
       title: "How much would you like?",
-      nextSection: "sectionFour",
+      nextSection: isCapsuleSelected ? "sectionFive" : "sectionFour",
       sidebar_name: "Quantity",
       activeIndex: null,
       hidden: true,
       cards: [
         {
-          id:"quarter",
+          id: "quarter",
           name: "250g",
           description:
             "Perfect for the solo drinker. Yields about 12 delicious cups",
         },
         {
-          id:"half",
+          id: "half",
           name: "500g",
           description:
             "Perfect option for a couple. Yields about 40 delectable cups",
         },
         {
-          id:"kilo",
+          id: "kilo",
           name: "1000g",
           description:
             "Perfect for offices and events. Yields about 90 delightful cups",
@@ -88,27 +90,26 @@ const Order = () => {
       ],
     },
     sectionFour: {
-      "value_key": "grind",
+      value_key: "grind",
       activeIndex: null,
       title: "Want us to grind them?",
       sidebar_name: "Grind Option",
       nextSection: "sectionFive",
       hidden: true,
-      disabledCondition: (quizState) => quizState.process === "capsule",
       cards: [
         {
-          id:"wholebean",
+          id: "wholebean",
           name: "Wholebean",
           description: "Best choice if you cherish the full sensory experience",
         },
         {
-          id:"filter",
+          id: "filter",
           name: "Filter",
           description:
             "For drip or pour-over coffee methods such as V60 or Aeropress",
         },
         {
-          id:"cafe",
+          id: "cafe",
           name: "Cafetiére",
           description:
             " Course ground beans specially suited for french press coffee",
@@ -116,7 +117,7 @@ const Order = () => {
       ],
     },
     sectionFive: {
-      "value_key": "frequency",
+      value_key: "frequency",
       activeIndex: null,
       title: "How often should we deliver?",
       sidebar_name: "Deliveries",
@@ -124,183 +125,270 @@ const Order = () => {
       nextSection: "END",
       cards: [
         {
-          id:"weekly",
+          id: "weekly",
           name: "Every week",
           description: "$7.20 per shipment. Includes free first-class shipping",
         },
         {
-          id:"bi_monthly",
+          id: "bi_monthly",
           name: "Every 2 weeks",
           description: "$9.60 per shipment. Includes free priority shipping",
         },
         {
-          id:"monthly",
+          id: "monthly",
           name: "Every month",
           description: "$12.00 per shipment. Includes free priority shipping",
         },
       ],
     },
   });
+  const quizSwitch = useCallback(() => {
+    switch (quizArrayLength) {
+      case 0:
+        return "sectionOne";
+      case 1:
+        return "sectionTwo";
+      case 2:
+        return "sectionThree";
+      case 3:
+        if (isCapsuleSelected) {
+          return "sectionFive";
+        } else {
+          return "sectionFour";
+        }
+      case 4:
+        return "sectionFive";
+      default:
+        return "sectionOne";
+    }
+  }, [isCapsuleSelected, quizArrayLength]);
+
+  useEffect(() => {
+    if (quizSwitch() === "sectionFive") {
+      setSectionCards({
+        ...sectionCards,
+        sectionFour: {
+          ...sectionCards.sectionFour,
+          hidden: true,
+        },
+        sectionFive: {
+          ...sectionCards.sectionFive,
+          hidden: false,
+        },
+      });
+    }
+    document
+      .getElementById(quizSwitch())
+      .scrollIntoView({ behavior: "smooth", block: "start" });
+  }, [isCapsuleSelected, quizSwitch]);
 
   // In the below onClickHandler, we are accessing the default state, reinserting it into the state via the
   // setSectionCards setter, then we are spreading what's inside the clicked section's contents within
   // the selected section. This is done so that we may update the active index within the appropriate section.
   const onClickHandler = (card, index, sectionName) => {
-    const section = sectionCards[sectionName]
-    const { nextSection, value_key } = section
+    const section = sectionCards[sectionName];
+    const { nextSection, value_key } = section;
+    if (sectionName === "sectionOne" && index === 0) {
+      setIsCapsuleSelected(true);
+    } else if (
+      (sectionName === "sectionOne" && index === 1) ||
+      (sectionName === "sectionOne" && index === 2)
+    ) {
+      setIsCapsuleSelected(false);
+    }
     const updateObject = {
       ...sectionCards,
       [sectionName]: {
         ...section,
         activeIndex: index,
-      }
-    }
-    if(nextSection!=="END") {
+      },
+    };
+
+    if (nextSection !== "END") {
       updateObject[nextSection] = {
         ...sectionCards[nextSection],
-        hidden: false
-      }
+        hidden: false,
+      };
     }
+
     setSectionCards(updateObject);
     setQuizState({
       ...quizState,
-      [value_key]: card.id
-    })
-      /**
-       * Hide current section //optional, check abprop/config
-       */
-
-      /**
-       * Unhide next section
-       */
-    if(nextSection === "END") {
+      [value_key]: card.id,
+    });
+    if (nextSection === "END") {
       //show summary
-    }
-    else onClickHideHandler(false, nextSection, true)
+    } else onClickHideHandler(false, nextSection, true);
   };
+
+  //TODO: remove the sectionFour choice selected from the quizState if capsule is selected
 
   // Toggles the "hidden" class that hides and unhides order sections
   const onClickHideHandler = (hidden, sectionName, skipStateSet) => {
-    if(!hidden) {
-      document
-        .getElementById(sectionName)
-        .scrollIntoView({ behavior: "smooth", block: "start" });
+    if (sectionName === "sectionFour" && isCapsuleSelected) {
+      return;
     }
-    if(!skipStateSet) setSectionCards({
-      ...sectionCards,
-      [sectionName]: {
-        ...sectionCards[sectionName],
-        hidden
-      },
+
+    if (!skipStateSet)
+      setSectionCards({
+        ...sectionCards,
+        [sectionName]: {
+          ...sectionCards[sectionName],
+          hidden,
+        },
+      });
+  };
+
+  const capsuleAndSecFour = (sectionName) => {
+    //  return text-gray-2 on sectionCards.sectionFour if capsule is selected
+    return sectionName === "sectionFour" && isCapsuleSelected;
+  };
+
+  const capsuleSideBar = (sectionName) => {
+    return capsuleAndSecFour(sectionName) ? "text-disabled" : "text-gray-2";
+  };
+
+  const handleSideBarClick = (section, sectionName) => {
+    onClickHideHandler((section.hidden = false), sectionName);
+    document.getElementById(sectionName).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
     });
   };
 
+  // BELOW IS THE RENDERING OF THE COMPONENT
+
   return (
     <section className="flex justify-center h-[2000px]">
-      <div className=" flex justify-center background-cream all-width">
+      <div className=" flex justify-center background-cream all-width text-">
         <div className="flex justify-center w-3/4">
-
-        
           <div className="flex flex-col gap-4 w-1/4 sticky h-[500px] top-20">
-          { Object.entries(sectionCards).map(([sectionName, section], index) => {
-            return (
-              <><span className="fraunces-700 text-gray-2 text-[24px] cursor-pointer">
-                { Number(index + 1).toLocaleString('en-US', {
-                    minimumIntegerDigits: 2,
-                    useGrouping: false
-                }) } &nbsp; &nbsp; { section.sidebar_name }
-              </span><div className=" border-b-2 border-x-gray-700"></div></>
-            )
-          }) }
+            {Object.entries(sectionCards).map(
+              ([sectionName, section], index) => {
+                return (
+                  <>
+                    <span
+                      onClick={() => handleSideBarClick(section, sectionName)}
+                      className={`fraunces-700 text-[24px] ${
+                        !capsuleAndSecFour(sectionName) ? "cursor-pointer" : ""
+                      } ${capsuleSideBar(sectionName)}`}
+                    >
+                      {Number(index + 1).toLocaleString("en-US", {
+                        minimumIntegerDigits: 2,
+                        useGrouping: false,
+                      })}
+                      &nbsp; &nbsp; {section.sidebar_name}
+                    </span>
+                    <div className=" border-b-2 border-x-gray-700"></div>
+                  </>
+                );
+              }
+            )}
           </div>
           <div className="flex flex-col pl-28 w-5/6 mb-16 ">
-            
             {/* START*/}
-            { Object.entries(sectionCards).map(([sectionName, section]) => {
-              return (
-                <><div className="flex mb-4 justify-between items-center" key={sectionName}>
-                  <h2
-                    className="text-gray fraunces-900 text-[40px] leading-[48px] "
-                    id={ sectionName }
-                  >
-                    { section.title }
-                  </h2>
-                  <div
-                    onClick={() => onClickHideHandler(
-                      !section.hidden,
-                      sectionName
-                    )}
-                    className="arrow-up cursor-pointer"
-                  >
-                    {/*Beautiful animation for the arrow to flip up and down using Framer*/}
-                    <motion.div
-                      initial={{ rotate: 0 }}
-                      animate={{ rotate: section.hidden ? 180 : 0 }}
-                      style={{ rotate: 0 }}
+            {Object.entries(sectionCards).map(
+              ([sectionName, section], index) => {
+                return (
+                  <>
+                    <div
+                      className="flex mb-4 justify-between items-center"
+                      key={sectionName}
                     >
-                      <svg
-                        width="19"
-                        height="13"
-                        xmlns="http://www.w3.org/2000/svg"
+                      <h2
+                        className={`${
+                          !capsuleAndSecFour(sectionName)
+                            ? "text-gray"
+                            : "text-disabled"
+                        }  fraunces-900 text-[40px] leading-[48px]`}
+                        id={sectionName}
                       >
-                        <path
-                          d="M15.949.586l2.828 2.828-9.096 9.096L.586 3.414 3.414.586l6.267 6.267z"
-                          fill="#0E8784"
-                          fillRule="nonzero" />
-                      </svg>
-                    </motion.div>
-                  </div>
-                </div><div className={`flex justify-center gap-6 mb-16`}>
-                    {section.cards.map((card, index) => {
-                      return (
-                        <AnimatePresence key={index.toString()}>
-                          {!section.hidden && (
-                            <motion.div
-                              key={index.toString()}
-                              initial={{
-                                y: 0,
-                                opacity: 0,
-                                height: 0,
-                              }}
-                              animate={{
-                                y: 0,
-                                opacity: 1,
-                                height: "",
-                              }}
-                              exit={{
-                                y: 0,
-                                opacity: 0,
-                                height: 0,
-                                overflow: "hidden",
-                              }}
-                              transition={{
-                                type: "Tween",
-                                duration: 0.5,
-                              }}
-                              onClick={() => onClickHandler(
-                                card,
-                                index,
-                                sectionName
-                              )}
-                              className={`${section.hidden && "hidden"} flex flex-col order-card-color hover:bg-orange-200  order-card-height rounded-lg cursor-pointer 
+                        {section.title}
+                      </h2>
+                      <div
+                        onClick={() => {
+                          onClickHideHandler(!section.hidden, sectionName);
+                        }}
+                        /* Below condition disables cursor pointer if capsule is selected */
+                        className={`arrow-up ${
+                          !capsuleAndSecFour(sectionName)
+                            ? "cursor-pointer"
+                            : ""
+                        }`}
+                      >
+                        {/*Beautiful animation for the arrow to flip up and down using Framer*/}
+                        <motion.div
+                          initial={{ rotate: 0 }}
+                          animate={{ rotate: section.hidden ? 180 : 0 }}
+                          style={{ rotate: 0 }}
+                        >
+                          <svg
+                            width="19"
+                            height="13"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M15.949.586l2.828 2.828-9.096 9.096L.586 3.414 3.414.586l6.267 6.267z"
+                              fill="#0E8784"
+                              fillRule="nonzero"
+                            />
+                          </svg>
+                        </motion.div>
+                      </div>
+                    </div>
+                    <div className={`flex justify-center gap-6 mb-16`}>
+                      {section.cards.map((card, index) => {
+                        return (
+                          <AnimatePresence key={index.toString()}>
+                            {!section.hidden && (
+                              <motion.div
+                                key={index.toString()}
+                                initial={{
+                                  y: 0,
+                                  opacity: 0,
+                                  height: 0,
+                                }}
+                                animate={{
+                                  y: 0,
+                                  opacity: 1,
+                                  height: "",
+                                }}
+                                exit={{
+                                  y: 0,
+                                  opacity: 0,
+                                  height: 0,
+                                  overflow: "hidden",
+                                }}
+                                transition={{
+                                  type: "Tween",
+                                  duration: 0.5,
+                                }}
+                                onClick={() =>
+                                  onClickHandler(card, index, sectionName)
+                                }
+                                className={`${
+                                  section.hidden && "hidden"
+                                } flex flex-col order-card-color hover:bg-orange-200  order-card-height rounded-lg cursor-pointer 
                         w-1/3 transition ease-linear duration-100 
-                        ${section.activeIndex === index &&
-                                "card-green"}
+                        ${section.activeIndex === index && "card-green"}
                         `}
-                            >
-                              <h3 className="text-blue fraunces-900 text-[24px] leading-[32px] mt-8 ml-8">
-                                {card.name}
-                              </h3>
-                              <p className="barlow-400 text-[16px] leading-[26px] pt-8 pl-6 pr-6">
-                                {card.description}
-                              </p>
-                            </motion.div>
-                          )}
-                        </AnimatePresence>
-                      );
-                    })}
-                  </div></>)
-            }) }
+                              >
+                                <h3 className="text-blue fraunces-900 text-[24px] leading-[32px] mt-8 ml-8">
+                                  {card.name}
+                                </h3>
+                                <p className="barlow-400 text-[16px] leading-[26px] pt-8 pl-6 pr-6">
+                                  {card.description}
+                                </p>
+                              </motion.div>
+                            )}
+                          </AnimatePresence>
+                        );
+                      })}
+                    </div>
+                  </>
+                );
+              }
+            )}
 
             {/* END */}
           </div>
